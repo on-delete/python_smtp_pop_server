a973ef83c2e7a9a94b197ffcb387a5a83e2bd0f5
on-delete <akr.tester@googlemail.com>
2016-01-30 19:47:37 +0100

    Update smtpserver.py

--- smtpserver.py
+++ smtpserver.py
@@ -11,19 +11,21 @@ class CustomSMTPServer(smtpd.SMTPServer):
         print 'Receiving message from:', peer
         print 'Message addressed from:', mailfrom
         print 'Message addressed to  :', rcpttos
-        #print 'Message length        :', len(data)
-	#print 'Data		     :', data
         
+        #Schleife fuer alle Empfaenger, die die email erhalten
 	for val in rcpttos:
 
+		#Falls der Ordner fuer den Empfaenger noch nicht angelegt ist, wird
+		#dies getan.
 		if not os.path.exists(val):
 			os.mkdir(val)
-	
+			
+		#datei bekommt aktuellen Zeitstempel als Namen	
 		filename = time.strftime("%d.%m.%Y %H:%M:%S").replace(' ', '')		
-
 
 		print 'LOG: Email wird in Datei ' + filename + ' gespeichert.'		
  
+ 		#datei wird geoeffnet, beschrieben mit der Email und geschlossen
 		path = val + "/" + filename
 		d = open(path, "w")
 		d.write(data)
@@ -35,4 +37,5 @@ class CustomSMTPServer(smtpd.SMTPServer):
 
 server = CustomSMTPServer(('0.0.0.0', 8888), None)
 
+#Der Server laueft in einer Schleife, damit er immer neue Emails empfangen kann.
 asyncore.loop()
